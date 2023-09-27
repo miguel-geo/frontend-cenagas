81debfd1ce9554a9d19973a11bf7b67865d31876
miguel-geo <miguelgeotrabajo@gmail.com>
2023-09-26 18:06:05 -0600

    validation_files

--- Menu/js/main.js
+++ Menu/js/main.js
@@ -1,4 +1,4 @@
-﻿var apiUrl = "http://localhost:82/backend-cenagas/public/api/"; // la url del api guardada en el config.json de la aplicacion
+﻿var apiUrl = "http://localhost/cenagas/backend/public/api/"; // la url del api guardada en el config.json de la aplicacion
 var ducto;
 var tramo;
 var area;
